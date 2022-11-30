f48dd8193cac38f6737b5ad96bef9829303655ff
DanielaParra <dparralp396@gmail.com>
2022-11-30 16:33:03 -0500

    Api call test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';  
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    axios({
+      url: "https://www.dnd5eapi.co/api/races/dragonborn",
+      method: "GET",
+      dataResponse: "json"
+    }).then((response) => {
+      console.log(response.data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing</h1>
     </div>
   );
 }
