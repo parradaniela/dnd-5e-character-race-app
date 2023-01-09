433ad9f2caa0331a4655e51b0cffed236b8e1ad2
DanielaParra <dparralp396@gmail.com>
2023-01-09 17:04:19 -0500

    Added ternary statement to hide the Results component before the user makes a selection

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,46 +3,51 @@ import Image from './Image.js';
 const Results = ({ name, age, raceIndex, alignment, sizeDesc, langDesc }) => {
     
     return (
+        
         <div className="results-main">
-            <div className="results-top">
-                <h2>{name}</h2>
-                <Image raceIndex={raceIndex} />
-            </div>
-            <ul>
-                <li>
-                    <div className="list-heading">
-                        <p>Age</p>
+            {/* Ternary statement to hide results page before user chooses a race */}
+            {raceIndex ? 
+                <>
+                    <div className="results-top">
+                        <h2>{name}</h2>
+                        <Image raceIndex={raceIndex} />
                     </div>
-                    <div className="list-details">
-                        <p>{age}</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="list-heading">
-                        <p>Alignment</p>
-                    </div>
-                    <div className="list-details">
-                        <p>{alignment}</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="list-heading">
-                        <p>Size</p>
-                    </div>
-                    <div className="list-details">
-                        <p>{sizeDesc}</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="list-heading">
-                        <p>Language</p>
-                    </div>
-                    <div className="list-details">
-                        <p>{langDesc}</p>
-                    </div>
-                </li>
-            </ul>
-
+                    <ul>
+                        <li>
+                            <div className="list-heading">
+                                <p>Age</p>
+                            </div>
+                            <div className="list-details">
+                                <p>{age}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-heading">
+                                <p>Alignment</p>
+                            </div>
+                            <div className="list-details">
+                                <p>{alignment}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-heading">
+                                <p>Size</p>
+                            </div>
+                            <div className="list-details">
+                                <p>{sizeDesc}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="list-heading">
+                                <p>Language</p>
+                            </div>
+                            <div className="list-details">
+                                <p>{langDesc}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </>
+            : null}
         </div>
     )
 }
