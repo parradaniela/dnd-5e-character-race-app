f2e63042db8feebc4a6869b84bac3bbd3128f5ec
DanielaParra <dparralp396@gmail.com>
2023-01-23 16:32:21 -0500

    Updated footer and added styling to active tab headings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="wrapper">
-                <p>Created at <a href="https://junocollege.com/">Juno College of Technology</a>. Background image by <a href="https://unsplash.com/@szmigieldesign">Lukasz Szmigiel</a>. All character portraits credit to <a href="https://www.dndbeyond.com/">DnD Beyond</a>.</p>
+                <p>Created with 💖 by <a href="https://danielaparra.com">Daniela Parra</a>. All character portraits credit to <a href="https://www.dndbeyond.com/">DnD Beyond</a>.</p>
             </div>
         </footer>
     )
